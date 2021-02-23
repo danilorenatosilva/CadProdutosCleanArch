9af0d85ad8ee001edd305630e8d53c251c8b9276
Danilo Renato da Silva <DaniloRenatoSilva@hotmail.com>
2021-02-23 15:45:48 -0300

    Implementando interfaces de repositório na camada de infraestrutura

---
+++ InfraCleanArch/Repositorios/CategoriaRepositorio.cs
@@ -0,0 +1,59 @@
+﻿using DominioCleanArch;
+using DominioCleanArch.Interfaces;
+using InfraCleanArch.Contexto;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InfraCleanArch.Repositorios
+{
+	public class CategoriaRepositorio : ICategoriaRepositorio
+	{
+		public readonly ProdutosDbContext _contexto;
+
+		public CategoriaRepositorio(ProdutosDbContext contexto)
+		{
+			_contexto = contexto;
+		}
+
+		public Categoria GetCategoriaById(int id)
+		{
+			return _contexto.Categorias.FirstOrDefault(p => p.Id == id);
+		}
+
+		public IEnumerable<Categoria> GetCategorias()
+		{
+			return _contexto.Categorias.ToList();
+		}
+
+		public Categoria Create(Categoria categoria)
+		{
+			_contexto.Add(categoria);
+			_contexto.SaveChanges();
+			return categoria;
+		}
+
+		public void Update(int id, Categoria categoria)
+		{
+			var categoriaToUpdate = GetCategoriaById(id);
+			if (categoriaToUpdate == null)
+			{
+				throw new Exception("Categoria não encontrado no banco de dados");
+			}
+			_contexto.Entry(categoria).State = EntityState.Modified;
+			_contexto.SaveChanges();
+		}
+
+		public void Delete(int id)
+		{
+			var categoria = GetCategoriaById(id);
+			if (categoria == null)
+			{
+				throw new Exception("Categoria não encontrado no banco de dados");
+			}
+			_contexto.Remove(categoria);
+		}
+	}
+}
---
+++ InfraCleanArch/Repositorios/ProdutoRepositorio.cs
@@ -0,0 +1,63 @@
+﻿using DominioCleanArch;
+using DominioCleanArch.Interfaces;
+using InfraCleanArch.Contexto;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace InfraCleanArch.Repositorios
+{
+	public class ProdutoRepositorio : IProdutoRepositorio
+	{
+		public readonly ProdutosDbContext _contexto;
+
+		public ProdutoRepositorio(ProdutosDbContext contexto)
+		{
+			_contexto = contexto;
+		}
+
+		public Produto GetProdutoById(int id)
+		{
+			return _contexto.Produtos.FirstOrDefault(p => p.Id == id);
+		}
+
+		public IEnumerable<Produto> GetProdutos()
+		{
+			return _contexto.Produtos.ToList();
+		}
+
+		public Produto Create(Produto produto)
+		{
+			_contexto.Add(produto);
+			_contexto.SaveChanges();
+			return produto;
+		}
+
+		public void Update(int id, Produto produto)
+		{
+			var produtoToUpdate = GetProdutoById(id);
+			if (produtoToUpdate == null)
+			{
+				throw new Exception("Produto não encontrado no banco de dados");
+			}
+			_contexto.Entry(produto).State = EntityState.Modified;
+			_contexto.SaveChanges();
+		}
+
+		public void Delete(int id)
+		{
+			var produto = GetProdutoById(id);
+			if (produto == null)
+			{
+				throw new Exception("Produto não encontrado no banco de dados");
+			}
+			_contexto.Remove(produto);
+		}
+
+		public IEnumerable<Produto> GetProdutosByIdCategoria(int idCategoria)
+		{
+			return _contexto.Produtos.Where(p => p.IdCategoria == idCategoria).ToList();
+		}
+	}
+}
