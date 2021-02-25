7e195c663078f4cf94d277144f08be9b309b7973
Danilo Renato da Silva <DaniloRenatoSilva@hotmail.com>
2021-02-24 23:53:03 -0300

    Adicionando ViewModel de Produto

---
+++ AplicacaoCleanArch/ViewModels/ProdutoViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace AplicacaoCleanArch.ViewModels
+{
+	public class ProdutoViewModel
+	{
+		public int Id { get; set; }
+		public string Nome { get; set; }
+		public string Descricao { get; set; }
+		public string UrlImagem { get; set; }
+		public decimal PrecoUnitario { get; set; }
+		public int IdCategoria { get; set; }
+		public IFormFile ArquivoImagem { get; set; }
+		public string CaminhoFisicoImagens { get; set; }
+	}
+}
