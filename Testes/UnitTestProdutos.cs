87a0c7a9130326c3f5f2cf5acf72ad788cc70eff
Danilo Renato da Silva <DaniloRenatoSilva@hotmail.com>
2021-02-27 17:51:16 -0300

    Ajustando teste unitário de produtos

--- Testes/UnitTestProdutos.cs
+++ Testes/UnitTestProdutos.cs
@@ -1,6 +1,7 @@
 using API.Controllers;
 using AplicacaoCleanArch.Interfaces;
 using AplicacaoCleanArch.ViewModels;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using System.Collections.Generic;
@@ -51,7 +52,61 @@ namespace Testes
             // Assert  
             Assert.IsAssignableFrom<ActionResult<IEnumerable<ProdutoViewModel>>>(result);
             Assert.True((okResult.Value as IEnumerable<ProdutoViewModel>).Count() > 0);
-
         }
+
+        [Fact]
+        public async Task Create_ProdutoFoiAdicionado()
+        {
+            //Arrange
+            var mockServico = new Mock<IProdutoServico>();
+            mockServico.Setup(mockServico => mockServico.Create(GetProdutosTeste()[0]))
+                        .Returns(GetProdutosTeste()[0]);
+            var controller = new ProdutosController(mockServico.Object);
+
+            //Act
+            var result = controller.Create(GetProdutosTeste()[0]);
+            var okResult = result.Result as OkObjectResult;
+
+            // Assert  
+            Assert.IsAssignableFrom<ActionResult<ProdutoViewModel>>(result);
+            Assert.NotNull((okResult.Value as ProdutoViewModel));
+        }
+
+        [Fact]
+        public async Task Update_ProdutoFoiAlterado()
+        {
+            //Arrange
+            var mockServico = new Mock<IProdutoServico>();
+            var produto = GetProdutosTeste()[0];
+            mockServico.Setup(mockServico => mockServico.Create(produto))
+                        .Returns(produto);
+            var controller = new ProdutosController(mockServico.Object);
+
+            //Act
+            produto.Nome = "Produto 1 alterado";
+            var result = controller.Update(produto);
+            var noContentResult = result as NoContentResult;
+
+            // Assert  
+            Assert.IsAssignableFrom<ActionResult>(result);
+            Assert.Equal(noContentResult.StatusCode, StatusCodes.Status204NoContent);
+        }
+
+        [Fact]
+        public async Task Delete_ProdutoFoiDeletado()
+        {
+            //Arrange
+            var mockServico = new Mock<IProdutoServico>();            
+            mockServico.Setup(mockServico => mockServico.Delete(3));
+            var controller = new ProdutosController(mockServico.Object);
+
+            //Act
+            var result = controller.Delete(3);
+
+            // Assert  
+            Assert.IsType<NoContentResult>(result);
+        }
+
+
     }
 }
